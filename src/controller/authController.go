fd321a2e11688e75f13c81147fcf874cc435ad59
hash <hashesandsalts000@gmail.com>
2024-04-14 23:52:24 +0545

    work on login and jwt

--- src/controller/authController.go
+++ src/controller/authController.go
@@ -1,47 +1,81 @@
 package controller
 
 import (
+	"errors"
 	"go-talk/src/database"
 	"go-talk/src/models"
-	// "go-talk/src/util"
-	"log"
+	"os"
 
-	// "log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt/v5"
+	"gorm.io/gorm"
 )
 
 // register user
 func UserRegisterHandler(c *gin.Context) {
 
-  var user models.User
-
+	var user models.User
 	if err := c.BindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	if err != nil {
+	if err := user.HashPassword(); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to hash password"})
 		return
 	}
 
-  log.Println("user Details: ", user)
-  user.Password = string(hashedPassword) 
-  log.Println("user Details: ", user)
+	if err := database.DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create user"})
+		return
+	}
 
-  if err := database.DB.Create(&user).Error; err != nil {
-    c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create user"})
-    return
-  }
-
-  c.JSON(http.StatusOK, map[string]string{"response": "user creation successful"})
+	c.JSON(http.StatusOK, gin.H{"success": "user creation successful"})
 }
 
 func UserLoginHandler(c *gin.Context) {
+
+	var user models.User
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := user.HashPassword(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	queryResult := database.DB.First(&user)
+	if queryResult.RowsAffected == 0 || errors.Is(queryResult.Error, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusOK, gin.H{"message": "invalid credentials"})
+		return
+	}
+
+	// jwt if found
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":    user.Email,
+		"username": user.Username,
+	})
+
+	tokenString, err := claims.SignedString([]byte(os.Getenv("JWT_SIGN_KEY")))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not sign jwt key"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+
+	c.SetCookie(
+		"token",
+		tokenString,
+		10,
+		"/",
+		"localhost",
+		true,
+		true,
+	)
 
 }
 
--- src/models/user.go
+++ src/models/user.go
@@ -1,11 +1,26 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
-  gorm.Model
-	FirstName string
-	LastName  string
-	Email     string
-	Password  string
+	gorm.Model
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Username  string `json:"username" gorm:"unique"`
+	Email     string `json:"userEmail" gorm:"unique"`
+	Password  string `json:"-"`
+}
+
+func (user *User) HashPassword() error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return err
+	}
+
+	user.Password = string(bytes)
+	return nil
 }
